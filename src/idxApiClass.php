4ea08893a854a4757a9463ab1b64202e75431d27
antonioortegajr <antonioortegajr@gmail.com>
2016-08-24 20:42:16 -0700

    added switch for http response
    added switch for all the status codes documented.

--- src/idxApiClass.php
+++ src/idxApiClass.php
@@ -25,10 +25,6 @@ class idxApiClass
       //Combine the component and API method
       $endpoint = $apiComponent.'/'.$apiMethod;
 
-      //create meta data about API call
-      $meta = array('endpoint'=>$endpoint,'method'=>$requestMethod);
-
-
       //Use Guzzle for API call and return json
       $client = new GuzzleHttp\Client(['base_url' => 'https://api.idxbroker.com']);
 
@@ -45,11 +41,54 @@ class idxApiClass
       );
       //http code return from above API call
       $code = $request->getStatusCode();
+      switch ($code) {
+        case 200:
+        $code = array($code => 'OK');
+        break;
+        case 204:
+        $code = array($code => 'We all good, nothing to return.') ;
+        break;
+        case 400:
+        $code = array($code => 'Required parameter missing or invalid. Check the API endpoint used.');
+        break;
+        case 401:
+        $code = array($code => 'accesskey not valid or revoked. You could reset the API key');
+        break;
+        case 403:
+        $code = array($code => 'Call not using SSL (HTTPS). This could be the hosting used to make the call.');
+        break;
+        case 404:
+        $code = array($code => 'The requested URL was not found on this server. Check the API endpoint you used.');
+        break;
+        case 406:
+        $code = array($code => 'No API Key provided.');
+        break;
+        case 409:
+        $code = array($code => 'Invalid API component specified or duplicate unique data detected.');
+        break;
+        case 412:
+        $code = array($code => 'Over Hourly API limit. Wait an hour and re check key in middleware.');
+        break;
+        case 417:
+        $code = array($code => 'Either over 1k in saved links created by API or no title in the saved links PUT request. Check response header for indication.');
+        break;
+        case 500:
+        $code = array($code => 'General error.');
+        break;
+        case 503:
+        $code = array($code=>'Scheduled or emergency API maintenance will result in 503 errors.');
+        break;
+        case 521:
+        $code = array($code => 'Temporary error. There is a possibility that not all API methods are affected.');
+        break;
+      }
 
+      //create meta data about this API call
+      $meta = array('code'=>$code,'request method'=>$requestMethod);
 
       $returnBody = $request->getBody();
 
-      return $code;
+      return $returnBody;
 
 
 
